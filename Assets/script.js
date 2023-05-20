d0b66d3717d7ecfe85a67909772eb2610ad378bf
SMarquis85 <sophienathalie_85@outlook.com>
2023-05-20 19:54:04 -0400

    updated js

--- Assets/script.js
+++ Assets/script.js
@@ -3,28 +3,30 @@ const questions = [
     {
       question: "Commonly used data types DO NOT include:",
       choices: ["strings", "booleans", "alerts", "numbers"],
-      answer: 
+      answer: "alerts",
     },
-    {
-      question: "The condition in an if / else statement is enclosed within ________.",
-      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: 
+    
+    { question: "The condition in an if / else statement is enclosed within ________.",
+      choices: ["quotes", "curly braces", "parentheses", "square braces"],
+      answer: "curly braces",
     },
+    
     {
       question: "Arrays in JavaScript can be used to store _______.",
       choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
-      answer: 
+      answer: "all of the above",
     },
     {
       question: "String values must be enclosed withtin ___________ when being assigned to variables.",
-      choices: ["commas", "curly brackets", "quotes", "parentheses"],
-      answer: 
+      choices: ["commas", "curly braces", "quotes", "parentheses"],
+      answer: "quotes",
     },
     {
       question: "A very useful tool used during development and debugging for printing content to the debugger is:",
       choices: ["JavaScript", "terminal/bash", "for loops", "console.log"],
-      answer: 
+      answer: "console.log",
     },
+
   ];
   
   const quizContainer = document.getElementById("quiz");
