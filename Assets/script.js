1e041f385bf1cf163882a31ea18a5acb6dd87d35
SMarquis85 <sophienathalie_85@outlook.com>
2023-05-21 13:59:31 -0400

    updated html,css and js

--- Assets/script.js
+++ Assets/script.js
@@ -28,120 +28,115 @@ const questions = [
     },
 
   ];
-  
-  const quizContainer = document.getElementById("quiz");
-  const startButton = document.getElementById("start");
-  const timerElement = document.getElementById("time");
-  const questionElement = document.getElementById("question");
-  const choicesContainer = document.getElementById("choices");
-  const feedbackElement = document.getElementById("feedback");
-  const highscoreForm = document.getElementById("highscore-form");
-  const initialsInput = document.getElementById("initials");
-  const highscoreList = document.getElementById("highscore-list");
-  
-  let currentQuestionIndex;
-  let timeLeft;
-  let timerInterval;
-  let highscores = [];
-  
-  // Start the quiz when the start button is clicked
-  startButton.addEventListener("click", startQuiz);
-  
-  // Submit high score when the form is submitted
-  highscoreForm.addEventListener("submit", submitHighscore);
-  
-  // Function to start the quiz
-  function startQuiz() {
-    currentQuestionIndex = 0;
-    timeLeft = 60; // Set the timer to 60 seconds
-  
-    quizContainer.style.display = "none";
-    startButton.style.display = "none";
-    highscoreForm.style.display = "none";
-    highscoreList.style.display = "none";
-    timerElement.textContent = timeLeft;
-  
-    // Start the timer
-    timerInterval = setInterval(updateTimer, 1000);
 
-   // Display the instructions
-document.getElementById("question").textContent = "Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score time by ten seconds.";
-  
-    // Display the first question
+  const quizContainer = document.getElementById("quiz");
+const startButton = document.getElementById("start");
+const timerElement = document.getElementById("time");
+const questionElement = document.getElementById("question");
+const choicesContainer = document.getElementById("choices");
+const feedbackElement = document.getElementById("feedback");
+const highscoreForm = document.getElementById("highscore-form");
+const initialsInput = document.getElementById("initials");
+const highscoreList = document.getElementById("highscore-list");
+
+let currentQuestionIndex;
+let timeLeft;
+let timerInterval;
+let highscores = [];
+
+// Start the quiz when the start button is clicked
+startButton.addEventListener("click", startQuiz);
+
+// Submit high score when the form is submitted
+highscoreForm.addEventListener("submit", submitHighscore);
+
+// Function to start the quiz
+function startQuiz() {
+  currentQuestionIndex = 0;
+  timeLeft = 60; // Set the timer to 60 seconds
+
+  quizContainer.style.display = "none";
+  startButton.style.display = "none";
+  highscoreForm.style.display = "none";
+  highscoreList.style.display = "none";
+  timerElement.textContent = timeLeft;
+
+  // Start the timer
+  timerInterval = setInterval(updateTimer, 1000);
+
+  // Display the first question
+  displayQuestion();
+}
+
+// Function to update the timer
+function updateTimer() {
+  timeLeft--;
+  timerElement.textContent = timeLeft;
+
+  if (timeLeft <= 0) {
+    // Quiz is over when the timer reaches 0
+    clearInterval(timerInterval);
+    endQuiz();
+  }
+}
+
+// Function to display a question
+function displayQuestion() {
+  const question = questions[currentQuestionIndex];
+  questionElement.textContent = question.question;
+  choicesContainer.innerHTML = "";
+
+  // Create and display the answer choices
+  for (let i = 0; i < question.choices.length; i++) {
+    const choice = question.choices[i];
+    const choiceButton = document.createElement("button");
+    choiceButton.textContent = choice;
+    choiceButton.addEventListener("click", handleChoiceClick);
+    choicesContainer.appendChild(choiceButton);
+  }
+}
+
+// Function to handle the user's choice
+function handleChoiceClick(event) {
+  const selectedChoice = event.target;
+  const question = questions[currentQuestionIndex];
+
+  if (selectedChoice.textContent === question.answer) {
+    // The answer is correct
+    feedbackElement.textContent = "Correct!";
+  } else {
+    // The answer is wrong
+    feedbackElement.textContent = "Wrong!";
+    timeLeft -= 10; // Penalty of 10 seconds for wrong answers
+    if (timeLeft < 0) {
+      timeLeft = 0;
+    }
+  }
+
+  currentQuestionIndex++;
+
+  if (currentQuestionIndex === questions.length) {
+    // All questions have been answered
+    clearInterval(timerInterval);
+    endQuiz();
+  } else {
     displayQuestion();
   }
-  
-  // Function to update the timer
-  function updateTimer() {
-    timeLeft--;
-    timerElement.textContent = timeLeft;
-  
-    if (timeLeft <= 0) {
-      // Quiz is over when the timer reaches 0
-      clearInterval(timerInterval);
-      endQuiz();
-    }
-  }
-  
-  // Function to display a question
-  function displayQuestion() {
-    const question = questions[currentQuestionIndex];
-    questionElement.textContent = question.question;
-    choicesContainer.innerHTML = "";
-  
-    // Create and display the answer choices
-    for (let i = 0; i < question.choices.length; i++) {
-      const choice = question.choices[i];
-      const choiceButton = document.createElement("button");
-      choiceButton.textContent = choice;
-      choiceButton.addEventListener("click", handleChoiceClick);
-      choicesContainer.appendChild(choiceButton);
-    }
-  }
-  
-  // Function to handle the user's choice
-  function handleChoiceClick(event) {
-    const selectedChoice = event.target;
-    const question = questions[currentQuestionIndex];
-  
-    if (question.answer === Array.from(choicesContainer.children).indexOf(selectedChoice)) {
-      // The answer is correct
-      feedbackElement.textContent = "Correct!";
-    } else {
-      // The answer is wrong
-      feedbackElement.textContent = "Wrong!";
-      timeLeft -= 10; // Penalty of 10 seconds for wrong answers
-      if (timeLeft < 0) {
-        timeLeft = 0;
-      }
-    }
-  
-    currentQuestionIndex++;
-  
-    if (currentQuestionIndex === questions.length) {
-      // All questions have been answered
-      clearInterval(timerInterval);
-      endQuiz();
-    } else {
-      displayQuestion();
-    }
-  }
-  
-  // Function to end the quiz
-  function endQuiz() {
-    quizContainer.style.display = "block";
-    feedbackElement.textContent = "";
-    questionElement.textContent = "All done!";
-    choicesContainer.innerHTML = "Your final score is: " + timeLeft;
+}
 
-  // Show the form to enter initials
+// Function to end the quiz
+function endQuiz() {
+  quizContainer.style.display = "block";
+  feedbackElement.textContent = "";
+  questionElement.textContent = "Quiz complete!";
+  choicesContainer.innerHTML = "";
   highscoreForm.style.display = "block";
 }
-  
-  // Function to submit the high score
-  function submitHighscore(event) {
-    event.preventDefault();
-    const initials = initialsInput.value.trim();
+
+// Function to submit the high score
+function submitHighscore(event) {
+  event.preventDefault();
+  const initials = initialsInput.value.trim();
 
   if (initials !== "") {
     const highscore = {
@@ -159,16 +154,16 @@ document.getElementById("question").textContent = "Try to answer the following c
     displayHighscores();
   }
 }
-  
-  // Function to display high scores
-  function displayHighscores() {
-    highscoreList.style.display = "block";
-    highscoreList.innerHTML = "";
-  
-    for (let i = 0; i < highscores.length; i++) {
-      const highscore = highscores[i];
-      const highscoreItem = document.createElement("div");
-      highscoreItem.textContent = `${highscore.initials}: ${highscore.score}`;
-      highscoreList.appendChild(highscoreItem);
-    }
+
+// Function to display high scores
+function displayHighscores() {
+  highscoreList.style.display = "block";
+  highscoreList.innerHTML = "";
+
+  for (let i = 0; i < highscores.length; i++) {
+    const highscore = highscores[i];
+    const highscoreItem = document.createElement("div");
+    highscoreItem.textContent = `${highscore.initials}: ${highscore.score}`;
+    highscoreList.appendChild(highscoreItem);
   }
+}
