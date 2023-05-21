b87a20064f2bb2042c428323c962b2f2dfc2877b
SMarquis85 <sophienathalie_85@outlook.com>
2023-05-20 20:06:16 -0400

    updated js and html

--- Assets/script.js
+++ Assets/script.js
@@ -63,6 +63,9 @@ const questions = [
   
     // Start the timer
     timerInterval = setInterval(updateTimer, 1000);
+
+    // Display the instructions
+  questionElement.textContent = "Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score time by ten seconds.";
   
     // Display the first question
     displayQuestion();
